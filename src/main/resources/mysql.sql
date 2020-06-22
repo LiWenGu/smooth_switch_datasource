03efadd78f2f2d0040207d4ee8e2aae8bce72fbd
liwenguang <liwenguang_dev@163.com>
2020-06-22 17:17:57 +0800

    增加表 create 语句，设置jdk8

---
+++ src/main/resources/mysql.sql
@@ -0,0 +1,11 @@
+-- A 表
+CREATE TABLE `test_a_table` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+-- B 表
+CREATE TABLE `test_b_table` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
